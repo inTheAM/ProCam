dd42fbd3cc12df690d30f214e5c89f41bf8c6a63
Ahmed Mgua <68146662+inTheAM@users.noreply.github.com>
2022-11-16 10:55:19 +0100

    Initial Commit

---
+++ ProCam/ProCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProCamApp.swift
+//  ProCam
+//
+//  Created by Ahmed Mgua on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ProCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
