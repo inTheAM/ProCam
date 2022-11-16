01d195aaaabdc0a56512f98ad9fb9e84551cfe3b
Ahmed Mgua <68146662+inTheAM@users.noreply.github.com>
2022-11-16 14:54:26 +0100

    Refactored camera preview

--- ProCam/ContentView.swift
+++ ProCam/ContentView.swift
@@ -10,18 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack(spacing: 0) {
-                #warning("Show camera preview")
-                Rectangle()
-                    .foregroundColor(.cyan.opacity(0.2))
-                    .overlay {
-                        Circle().stroke()
-                            .foregroundColor(.gray)
-                            .frame(width: 32, height: 32)
-                    }
+            cameraPreview
         }
         .ignoresSafeArea()
     }
     
+    #warning("Show camera preview")
+    var cameraPreview: some View {
+        Rectangle()
+            .foregroundColor(.cyan.opacity(0.2))
+            .overlay {
+                Circle().stroke()
+                    .foregroundColor(.gray)
+                    .frame(width: 32, height: 32)
+            }
+    }
     
 }
 
