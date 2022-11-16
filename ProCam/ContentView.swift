c3ef0f6c54c584dbbc7a6b453faac668f24041a3
Ahmed Mgua <68146662+inTheAM@users.noreply.github.com>
2022-11-16 14:49:23 +0100

    Added dummy camera preview with horizontal balance overlay

--- ProCam/ContentView.swift
+++ ProCam/ContentView.swift
@@ -9,18 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 0) {
+                #warning("Show camera preview")
+                Rectangle()
+                    .foregroundColor(.cyan.opacity(0.2))
+                    .overlay {
+                        Circle().stroke()
+                            .foregroundColor(.gray)
+                            .frame(width: 32, height: 32)
+                    }
         }
-        .padding()
+        .ignoresSafeArea()
     }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
