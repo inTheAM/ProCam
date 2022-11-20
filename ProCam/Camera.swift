30c2284dae18b01df7f6fdc66804a288bddfe2df
Ahmed Mgua <68146662+inTheAM@users.noreply.github.com>
2022-11-20 15:43:35 +0100

    Added Camera module to interface with UI

---
+++ ProCam/Camera.swift
@@ -0,0 +1,35 @@
+//
+//  Camera.swift
+//  ProCam
+//
+//  Created by Ahmed Mgua on 20/11/22.
+//
+
+import SwiftUI
+
+@MainActor
+final class Camera: ObservableObject {
+    private let service = CameraService()
+    @Published var preview: Image?
+    @Published var thumbnailImage: Image?
+    
+    init() {
+        Task {
+            await handleCameraPreviews()
+        }
+    }
+    
+    func handleCameraPreviews() async {
+        let imageStream = service.previewStream
+            .map { $0.image }
+
+        for await image in imageStream {
+            preview = image
+        }
+    }
+    
+    func start() async {
+        await service.start()
+    }
+    
+}
