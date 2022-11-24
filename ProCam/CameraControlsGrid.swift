46b2ab38e3e748ebbb4862b26988c3a24f1efe6e
Ahmed Mgua <68146662+inTheAM@users.noreply.github.com>
2022-11-24 18:16:27 +0100

    Rearranged controls into a paged tab view

--- ProCam/CameraControlsGrid.swift
+++ ProCam/CameraControlsGrid.swift
@@ -11,13 +11,6 @@ import SwiftUI
 /// Implemented as a Vertical Stack containing the drag indicator and the grid of controls.
 /// The grid of controls is initally offset downards so that only the top row is visible in the camera preview
 struct CameraControlsGrid: View {
-    /// The vertical offset for the view.
-    /// Initialized at 68 to offset the view downwards.
-    @State private var offset: CGFloat = 68
-    
-    /// Detects when the state of the application changes eg when the user moves it to the background, the app becomes inactive or the user switches back to the app.
-    @Environment(\.scenePhase) var scenePhase
-    
     @EnvironmentObject var hapticFeedback: HapticFeedback
     @EnvironmentObject var camera: Camera
     
@@ -26,116 +19,54 @@ struct CameraControlsGrid: View {
     @State private var isShowingWhiteBalanceSettings = false
     
     var body: some View {
-        VStack(spacing: 4) {
-            dragIndicator
-            
-            LazyVGrid(columns: [GridItem].init(repeating: GridItem(.flexible()), count: 5), spacing: 40) {
-                histogramButton
-                Spacer()
-                flashlightButton
-                Spacer()
-                gridOverlayButton
+        TabView {
                 if isShowingTimerSettings {
-                    timerSettings
-                        .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .scale))
+                    HStack {
+                        timerSettings
+                            .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .scale))
+                    }
                 } else if isShowingWhiteBalanceSettings {
-                    whiteBalanceSettings
-                        .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .scale))
+                    HStack {
+                        whiteBalanceSettings
+                            .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .scale))
+                    }
                 } else {
-                    Group {
+                    HStack {
                         switchFrontRearCameraButton
-                        heicRawButton
+                        Spacer()
+                        gridOverlayButton
+                        Spacer()
+                        flashlightButton
+                        Spacer()
                         timerButton
+                        Spacer()
                         whiteBalanceButton
-                        settingsButton
                     }
                     .transition(.scale)
-                }
-            }
-            .padding(.vertical)
-            .animation(.default, value: isShowingTimerSettings)
-            .animation(.default, value: isShowingWhiteBalanceSettings)
-        }
-        .padding(.horizontal)
-        .background {
-            LinearGradient(colors: [.clear, .black.opacity(0.2), .clear], startPoint: .top, endPoint: .bottom)
-        }
-        // Simultaneous Gesture is used to attach gestures to views which already have a gesture attached.
-        // For example, the grid elements are buttons, which already recognize tap gestures.
-        // To attach a drag gesture to the buttons, `.simultaneousGesture` is preferred to `.gesture`
-        .simultaneousGesture(
-            
-            // Declare a drag gesture with a minimum distance.
-            // A minimum distance of 0 means taps will also be recognized.
-            DragGesture(minimumDistance: 10)
-            // When the user drags on the screen, the changes are received here.
-                .onChanged { value in
-                    // The distance the drag gesture has covered thus far
-                    let distance = value.translation.height * 0.5
+                    .padding()
                     
-                    // Setting the allowed distance for which the drag gesture will be used to perform an action, eg offsetting a view.
-                    let allowedDistance = max(min(distance, 68), 0)
-                    
-                    // Assigning the allowable distance to the view offset
-                    offset = allowedDistance
-                    
-                }
-            // When the drag gesture is over, we receive data about the entire drag here.
-                .onEnded { value in
-                    // If the current offset is above 20, offset the view down to 68
-                    if offset < 20 {
-                        offset = 0
-                        hapticFeedback.playTick(isEmphasized: true)
-                        // If the current offset is less than 20, offset the view up to 0
-                    } else {
-                        offset = 68
-                        isShowingWhiteBalanceSettings = false
-                        isShowingTimerSettings = false
-                        hapticFeedback.playTick(isEmphasized: true)
+                    HStack  {
+                        heicRawButton
+                        Spacer()
+                        histogramButton
+                        Spacer()
+                        settingsButton
+                        Spacer()
+                        Spacer()
                     }
+                    .transition(.scale)
+                    .padding()
                 }
-        )
-        .animation(.default, value: offset)
-        .offset(y: offset)
-        // Detect when the application goes to the background or becomes inactive and reset the offset
-        .onChange(of: scenePhase) { newScenePhase in
-            if newScenePhase == .background || newScenePhase == .inactive {
-                offset = 68
-            }
         }
+        .frame(height: 48)
+        .tabViewStyle(.page(indexDisplayMode: .never))
+        .animation(.default, value: isShowingTimerSettings)
+        .animation(.default, value: isShowingWhiteBalanceSettings)
+        
     }
 }
 
 extension CameraControlsGrid {
-    
-    // MARK: - Drag Indicator
-    
-    /// The drag indicator
-    var dragIndicator: some View {
-        HStack(spacing: -4) {
-            RoundedRectangle(cornerRadius: 4)
-                .frame( height: 4)
-                .foregroundColor(.gray)
-                .rotationEffect(dragIndicatorAngle)
-            RoundedRectangle(cornerRadius: 4)
-                .frame( height: 4)
-                .foregroundColor(.gray)
-                .rotationEffect(-dragIndicatorAngle)
-        }
-        .frame(width: 32)
-    }
-    
-    /// The angle by which the rectangle should be rotated by the view.
-    var dragIndicatorAngle: Angle {
-        // Calculate the distance between the current position and the maximum offset for the view ie 68
-        let currentOffset = 68 - offset
-        
-        // Use the calculated distance to calclate the angle between 0 and 30 degrees the rectangle should be rotated to
-        let angle = currentOffset/68 * 20
-        
-        // Return the calculated angle
-        return Angle(degrees: angle)
-    }
     
     // MARK: - Grid Elements
     
@@ -222,7 +153,6 @@ extension CameraControlsGrid {
     /// A button that shows the timer settings.
     var timerButton: some View {
         Button {
-            #warning("Show timer settings")
             isShowingTimerSettings = true
         } label: {
             Image(systemName: "timer")
@@ -302,7 +232,6 @@ extension CameraControlsGrid {
     /// A button that shows white balance options.
     var whiteBalanceButton: some View {
         Button {
-            #warning("Show white balance modes")
             isShowingWhiteBalanceSettings = true
         } label: {
             Text("AWB")
